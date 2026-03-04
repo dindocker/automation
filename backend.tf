28f500456192cf7977b78bc7344d4ea1d93e5955
dindocker <ahmdeen1977@gmail.com>
2026-03-04 15:28:53 -0500

    Update and rename backend.tf to b.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstaten10007505RG"
-    storage_account_name = "tfstaten10007505sa"
-    container_name       = "tfstatefiles"
-    key                  = "assignment1-7505.tfstate"
-  }
-}
